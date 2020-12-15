2978eb70999e2ae33abcf7ae6ad57eea287ec1eb
Pedro Henrique Alves <63965249+phalvesr@users.noreply.github.com>
2020-12-15 12:38:33 -0300

    fix: indentation error

--- backend/src/javascrit-modules/puppeteer.js
+++ backend/src/javascrit-modules/puppeteer.js
@@ -8,18 +8,18 @@ async function startPuppeteer(searchTerms) {
    });
     let headline = []
     headline = searchTerms.map(async (element) => {
-    const page = await browser.newPage();
-    await page.goto(`https://www.bing.com/news/search?q=${element}&qs=n&form=QBNT&sp=-1&pq=brasil&sc=8-6&sk=&cvid=8BD35AE65AAA48CF948A98F4F2F286CF`);
+      const page = await browser.newPage();
+      await page.goto(`https://www.bing.com/news/search?q=${element}&qs=n&form=QBNT&sp=-1&pq=brasil&sc=8-6&sk=&cvid=8BD35AE65AAA48CF948A98F4F2F286CF`);
 
-    return page.evaluate(() => {
-      return {
-        title: document.querySelector('.title').textContent,
-        link:  document.querySelector('.title').href,
-        image: document.querySelector('img[class=rms_img]').src
-      }
+      return page.evaluate(() => {
+        return {
+          title: document.querySelector('.title').textContent,
+          link:  document.querySelector('.title').href,
+          image: document.querySelector('img[class=rms_img]').src
+        }
+      })
+
     })
-
-  })
 
   await Promise.all(headline);
   await browser.close();
